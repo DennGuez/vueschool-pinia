a93036a6d66549b04228e38dc577df65cadf2979
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2023-11-04 00:29:14 -0400

    ADVANCED: Subscribe to the State

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref, reactive } from "vue";
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "./stores/ProductStore";
@@ -6,6 +7,26 @@ import { useCartStore } from "./stores/CartStore";
 
 const productStore = useProductStore();
 const cartStore = useCartStore();
+const history = reactive([])
+const doingHistory = ref(false)
+
+history.push(JSON.stringify(cartStore.$state))
+
+const undo = () => {
+   if(history.length === 1) return
+   doingHistory.value = true
+   history.pop()
+   cartStore.$state = JSON.parse(history.at(-1))
+   doingHistory.value = false
+}
+
+cartStore.$subscribe((mutation, state) => {
+   // console.log({mutation});
+   // console.log({state});
+   if(!doingHistory.value) {
+      history.push(JSON.stringify(state))
+   }
+})
 
 cartStore.$onAction(({
    name,
@@ -31,6 +52,9 @@ productStore.fill();
 <template>
    <div class="container">
       <TheHeader />
+      <div class="mb-5 flex justify-end">
+         <AppButton @click=undo>Undo</AppButton>
+      </div>
       <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
          <ProductCard v-for="product in productStore.products" :key="product.name" :product="product"
             @addToCart="cartStore.addItems($event, product)" />
