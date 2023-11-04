84d72a8ff8825363c060f5abad98b2f4daf3048e
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2023-11-04 14:03:32 -0400

    ADVANCES: Pinia store persistence in local storage

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,5 @@
 import { defineStore, acceptHMRUpdate } from "pinia";
+import { useLocalStorage } from '@vueuse/core';
 import { groupBy } from "lodash";
 import { useAuthUserStore } from "./AuthUserStore";
 
@@ -6,7 +7,9 @@ export const useCartStore = defineStore('CartStore', {
    historyEnabled: true,
    state: () => {
       return {
-         items: [],
+         // items: [],
+         /* Usando datos persistentes en el localStora con vueuse */
+         items: useLocalStorage("CartStore:items", []),
       }
    },
 
