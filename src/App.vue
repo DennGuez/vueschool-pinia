53c49c5587c27447aada0ac2362bfe29f285be91
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2023-11-04 00:43:46 -0400

    ADVANCED: Subscribe to the State (Part 2)

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,25 @@ import { useCartStore } from "./stores/CartStore";
 const productStore = useProductStore();
 const cartStore = useCartStore();
 const history = reactive([])
+const future = reactive([])
 const doingHistory = ref(false)
 
 history.push(JSON.stringify(cartStore.$state))
 
+const redo = (params) => {
+   const latestState = future.pop()
+   if(!latestState) return;
+   doingHistory.value = true
+   history.push(latestState)
+   cartStore.$state = JSON.parse(latestState)
+   doingHistory.value = false
+}
+
 const undo = () => {
    if(history.length === 1) return
    doingHistory.value = true
-   history.pop()
+   // history.pop()
+   future.push(history.pop())
    cartStore.$state = JSON.parse(history.at(-1))
    doingHistory.value = false
 }
@@ -54,6 +65,7 @@ productStore.fill();
       <TheHeader />
       <div class="mb-5 flex justify-end">
          <AppButton @click=undo>Undo</AppButton>
+         <AppButton class="ml-2" @click=redo>Redo</AppButton>
       </div>
       <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
          <ProductCard v-for="product in productStore.products" :key="product.name" :product="product"
